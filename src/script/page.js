a394cf8ab4f62cc1ef0072a71fbb91079fd9ab75
Sanjeev Kaushik <sanjeevkaushik90684@gmail.com>
2026-01-16 21:05:39 +0530

    pae 3 done

--- src/script/page.js
+++ src/script/page.js
@@ -1,4 +1,25 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+
+
+var image = document.querySelector("#imageContainer")
+var fixed = document.querySelector("#fixedImage")
+image.addEventListener("mouseenter", function () {
+    fixed.style.display = "block"
+})
+image.addEventListener("mouseleave", function () {
+    fixed.style.display = "none"
+})
+
+
+var imageCards = document.querySelectorAll('.imageElement')
+
+imageCards.forEach(function (card) {
+    card.addEventListener("mouseenter", function () {
+        var imageNew = card.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${imageNew})`
+    })
+})
